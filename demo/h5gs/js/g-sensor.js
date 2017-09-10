bcb6b504b207249b4285137b395e51cc2d13bc26
Adele <491796203@qq.com>
2017-09-10 22:52:42 +0800

    Site updated: 2017-09-10 22:52:41

--- demo/h5gs/js/g-sensor.js
+++ demo/h5gs/js/g-sensor.js
@@ -1,6 +1,6 @@
 window.addEventListener("deviceorientation", function(event) {
     // 处理event.alpha、event.beta及event.gamma
-    alert(event.alpha)
-    alert(event.beta)
-    alert(event.gamma)
+    alert("alpha" + event.alpha)
+    alert("event" + event.beta)
+    alert("gamma" + event.gamma)
 }, true);

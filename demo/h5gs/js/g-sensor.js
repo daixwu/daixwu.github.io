d209ba3c99cfd8213fbaf9406111b8e8a435896c
Adele <491796203@qq.com>
2017-09-10 23:29:13 +0800

    Site updated: 2017-09-10 23:29:12

--- demo/h5gs/js/g-sensor.js
+++ demo/h5gs/js/g-sensor.js
@@ -1,6 +1,12 @@
-window.addEventListener("deviceorientation", function(event) {
-    // 处理event.alpha、event.beta及event.gamma
-    alert("alpha" + event.alpha)
-    alert("event" + event.beta)
-    alert("gamma" + event.gamma)
+// window.addEventListener("deviceorientation", function(event) {
+//     // 处理event.alpha、event.beta及event.gamma
+//     alert("alpha" + event.alpha)
+//     alert("event" + event.beta)
+//     alert("gamma" + event.gamma)
+// }, true);
+
+
+window.addEventListener("compassneedscalibration", function(event) {
+    alert('您的罗盘需要校准，请将设备沿数字8方向移动。');
+    event.preventDefault();
 }, true);

3a567cd7f383557092625c0b819369e9adf1e8ae
Adele <491796203@qq.com>
2017-09-10 23:34:13 +0800

    Site updated: 2017-09-10 23:34:13

--- demo/h5gs/js/g-sensor.js
+++ demo/h5gs/js/g-sensor.js
@@ -6,7 +6,17 @@
 // }, true);
 
 
-window.addEventListener("compassneedscalibration", function(event) {
-    alert('您的罗盘需要校准，请将设备沿数字8方向移动。');
-    event.preventDefault();
+// window.addEventListener("compassneedscalibration", function(event) {
+//     alert('您的罗盘需要校准，请将设备沿数字8方向移动。');
+//     event.preventDefault();
+// }, true);
+
+
+window.addEventListener("devicemotion", function(event) {
+    // 处理event.acceleration、event.accelerationIncludingGravity、
+    // event.rotationRate和event.interval
+    alert("第一个" + event.acceleration)
+    alert("第二个" + event.accelerationIncludingGravity)
+    alert("第三个" + event.rotationRate)
+    alert("第四个" + event.event.interval)
 }, true);
